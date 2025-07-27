69f3986396742e9c8f8175f6c4df7252bbd6029c
Hong Phuoc <hongphuoc6104@users.noreply.github.com>
2025-07-27 13:28:49 +0700

    bản sửa lỗi

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL 
+  baseURL: 'http://localhost:5000/api'
 });
 
 export default apiClient;
+
+// import.meta.env.VITE_API_URL
